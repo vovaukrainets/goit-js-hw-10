2723b12ac2600ae2b642c486022a5cd562e27b46
vovaukrainets <vovaukrainets@gmail.com>
2022-01-11 11:47:30 +0200

    Homework

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,6 @@
 export function fetchCountries(name) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
-  )
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,
+    capital,population,flags,languages`)
     .then(response => response.json())
     .catch(error => console.log(error));
 }
